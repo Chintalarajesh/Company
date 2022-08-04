80e26c9ef44644a4a520334883fed5b14151598d
Chintalarajesh <109692986+Chintalarajesh@users.noreply.github.com>
2022-08-04 15:52:45 +0530

    First Commit

---
+++ Company/src/Rajesh/First_Commit.java
@@ -0,0 +1,11 @@
+package Rajesh;
+
+public class First_Commit {
+int a= 10;
+void m1() {
+	int b = 20;
+	System.out.println(b);
+	System.out.println(a);
+}
+	
+}
---
+++ kanerikas/src/kanerikas/Raj.java
@@ -0,0 +1,7 @@
+package kanerikas;
+
+public class Raj {
+public static void main(String[] args) {
+	System.out.println("Rajesh");
+}
+}
